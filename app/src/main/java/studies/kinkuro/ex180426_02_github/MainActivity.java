51f0447b86115c37e02a58c0d78faf230d7d20b2
KinKuro <kinkuro88@gmail.com>
2018-04-26 14:34:40 +0900

    commit by master in alfo6-2

--- app/src/main/java/studies/kinkuro/ex180426_02_github/MainActivity.java
+++ app/src/main/java/studies/kinkuro/ex180426_02_github/MainActivity.java
@@ -1,13 +1,21 @@
 package studies.kinkuro.ex180426_02_github;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.tv);
+        tv.setText("Hello Universe");
+        tv.setTextColor(Color.DKGRAY);
     }
 }
