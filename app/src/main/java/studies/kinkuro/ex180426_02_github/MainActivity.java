0f3362311ad571daf5fb99d1d3f2b7479d1ecfce
KinKuro <kinkuro88@gmail.com>
2018-04-26 14:30:18 +0900

    Initial commit

---
+++ app/src/main/java/studies/kinkuro/ex180426_02_github/MainActivity.java
@@ -0,0 +1,13 @@
+package studies.kinkuro.ex180426_02_github;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
